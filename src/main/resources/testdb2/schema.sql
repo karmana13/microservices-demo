1e07297cfcbd66c67d3d65fc1d796f634e9d18be
karmanatrivedi <trivedi.karama@gmail.com>
2021-03-28 15:11:44 -0700

    added default thread in schema

--- src/main/resources/testdb2/schema.sql
+++ src/main/resources/testdb2/schema.sql
@@ -2,3 +2,5 @@ drop table T_POST if exists;
 
 create table T_POST (ID bigint identity primary key, NUMBER varchar(9), THREAD varchar(9),
                          SUBJECT varchar(50) not null, BODY varchar(100), unique(NUMBER));
+
+ALTER TABLE T_POST ALTER COLUMN THREAD SET DEFAULT 666666666;
