09450b9299aeba31910901ee02658463b3324625
karmanatrivedi <trivedi.karama@gmail.com>
2021-03-28 23:47:09 -0700

    post database changes

--- src/main/resources/testdb2/schema.sql
+++ src/main/resources/testdb2/schema.sql
@@ -1,6 +1,6 @@
 drop table T_POST if exists;
 
 create table T_POST (ID bigint identity primary key, NUMBER varchar(9), THREAD varchar(9),
-                         SUBJECT varchar(50) not null, BODY varchar(100));
+                        SUBJECT varchar(100) not null, BODY varchar(200));
 
-ALTER TABLE T_POST ALTER COLUMN THREAD SET DEFAULT 666666666;
+ALTER TABLE T_POST ALTER COLUMN THREAD SET DEFAULT 111111111;
